45d35d18776d234d620aa22eeec19356f133abd7
naimuddin94 <developernaimuddin@gmail.com>
2024-05-17 11:59:38 +0600

    update student.controller.ts

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -1,13 +1,44 @@
 import { Request, Response } from 'express';
-import { ApiResponse, asyncHandler } from '../../../utils';
-import { saveStudentToDB } from './student.service';
+import { ApiError, ApiResponse, asyncHandler } from '../../../utils';
+import { studentService } from './student.service';
 
-export const createStudent = asyncHandler(
+const createStudent = asyncHandler(async (req: Request, res: Response) => {
+  const { student } = req.body;
+  const result = await studentService.saveStudentToDB(student);
+  res
+    .status(200)
+    .json(new ApiResponse(200, result, 'Student created successfully'));
+});
+
+const fetchedStudents = asyncHandler(async (req: Request, res: Response) => {
+  const result = await studentService.fetchedStudentsToDB();
+
+  if (!result) {
+    throw new ApiError(404, 'Student not found');
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, result, 'Students retrieved successfully'));
+});
+
+const fetchedSingleStudent = asyncHandler(
   async (req: Request, res: Response) => {
-    const student = req.body;
-    const result = await saveStudentToDB(student);
-    res
+    const { id } = req.params;
+    const result = await studentService.fetchedSingleStudentToDB(id);
+
+    if (!result) {
+      throw new ApiError(404, 'Student not found');
+    }
+
+    return res
       .status(200)
-      .json(new ApiResponse(200, result, 'Student created successfully'));
+      .json(new ApiResponse(200, result, 'Student retrieved successfully'));
   },
 );
+
+export const studentController = {
+  createStudent,
+  fetchedStudents,
+  fetchedSingleStudent,
+};
