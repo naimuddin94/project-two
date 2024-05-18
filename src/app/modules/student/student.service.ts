f9989e803d91c0846d3f02a688b2970b67e89918
naimuddin94 <developernaimuddin@gmail.com>
2024-05-19 01:36:25 +0600

    update student.service.ts

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -1,20 +1,31 @@
+import { ApiError } from '../../../utils';
 import { IStudent } from './student.interface';
 import Student from './student.model';
 
-const saveStudentToDB = async (student: IStudent) => {
-  return await Student.create(student);
+const saveStudentToDB = async (studentData: IStudent) => {
+  if (await Student.isUserExists(studentData.id)) {
+    throw new ApiError(400, 'Student id already exists');
+  }
+  const result = await Student.create(studentData);
+  return result;
+};
+
+const deleteStudentFromDB = async (id: string) => {
+  const result = await Student.findByIdAndUpdate(id, { isDeleted: true });
+  return result;
 };
 
 const fetchedStudentsToDB = async () => {
-  return await Student.find();
+  return await Student.find().select('-password -isDeleted');
 };
 
 const fetchedSingleStudentToDB = async (id: string) => {
-  return await Student.findOne({ id });
+  return await Student.findOne({ id }).select('-password -isDeleted');
 };
 
 export const studentService = {
   saveStudentToDB,
   fetchedStudentsToDB,
   fetchedSingleStudentToDB,
+  deleteStudentFromDB,
 };
