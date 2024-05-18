b307fec9dddb2996493f167f7fd601246e9903e6
naimuddin94 <developernaimuddin@gmail.com>
2024-05-19 01:36:14 +0600

    update student.interface.ts

--- src/app/modules/student/student.interface.ts
+++ src/app/modules/student/student.interface.ts
@@ -1,21 +1,55 @@
-export interface IGuardian{
-    fatherName: string;
-    fatherOccupation: string;
-    motherName: string;
-    motherOccupation: string;
-}
+import { Model } from 'mongoose';
+import { z } from 'zod';
+import {
+  guardianValidationSchema,
+  studentValidationSchema,
+} from './student.validator';
 
-export interface IStudent {
-    id: string;
-    name: {
-        firstName: string;
-        lastName: string;
-    },
-    email: string;
-    dateOfBirth: Date;
-    address: string;
-    contactNo: string;
-    bloodGroup?: "A+" | "A-" | "B+" | "B-" | "AB+" | "AB-" | "O+" | "O-";
-    guardian: IGuardian;
-    isActive: boolean;
+// export interface IGuardian extends Document {
+//   fatherName: string;
+//   fatherOccupation: string;
+//   motherName: string;
+//   motherOccupation: string;
+// }
+
+// export interface IStudent extends Document {
+//   id: string;
+//   name: {
+//     firstName: string;
+//     lastName: string;
+//   };
+//   email: string;
+//   dateOfBirth: Date;
+//   address: string;
+//   contactNo: string;
+//   bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+//   guardian: IGuardian;
+//   isActive: boolean;
+// }
+
+// export type IGuardian = z.infer<typeof guardianValidationSchema>;
+// export type IStudent = z.infer<typeof studentValidationSchema>;
+
+export interface IGuardian extends z.infer<typeof guardianValidationSchema> {}
+export interface IStudent extends z.infer<typeof studentValidationSchema> {}
+
+// export type IStudentMethods = {
+//   // eslint-disable-next-line no-unused-vars
+//   isUserExists: (id: string) => Promise<IStudent | null>;
+// };
+
+// export interface IStudentMethods {
+//   // eslint-disable-next-line no-unused-vars
+//   isUserExists(id: string): Promise<IStudent | null>;
+// }
+
+// export type IStudentModel = Model<
+//   IStudent,
+//   Record<string, never>,
+//   IStudentMethods
+// >;
+
+export interface IStudentModel extends Model<IStudent> {
+  // eslint-disable-next-line no-unused-vars
+  isUserExists(id: string): Promise<IStudent | null>;
 }
