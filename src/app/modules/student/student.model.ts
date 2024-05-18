907283c8b7abe0e7ae6caf98febf081517b29f6f
naimuddin94 <developernaimuddin@gmail.com>
2024-05-19 01:36:18 +0600

    update student.model.ts

--- src/app/modules/student/student.model.ts
+++ src/app/modules/student/student.model.ts
@@ -1,5 +1,7 @@
+import bcrypt from 'bcrypt';
 import { Schema, model } from 'mongoose';
-import { IGuardian, IStudent } from './student.interface';
+import config from '../../config';
+import { IGuardian, IStudent, IStudentModel } from './student.interface';
 
 const GuardianSchema = new Schema<IGuardian>({
   fatherName: {
@@ -20,7 +22,7 @@ const GuardianSchema = new Schema<IGuardian>({
   },
 });
 
-const StudentSchema = new Schema<IStudent>({
+const studentSchema = new Schema<IStudent, IStudentModel>({
   id: {
     type: String,
     required: true,
@@ -40,6 +42,14 @@ const StudentSchema = new Schema<IStudent>({
     unique: true,
     lowercase: true,
     required: true,
+    // validate: function (this: IStudent, value: string) {
+    //   const last = this.name.lastName;
+    //   console.log({ value, last });
+    // },
+  },
+  password: {
+    type: String,
+    required: true,
   },
   dateOfBirth: {
     type: Date,
@@ -51,18 +61,69 @@ const StudentSchema = new Schema<IStudent>({
   },
   bloodGroup: {
     type: String,
+    enum: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
   },
   contactNo: {
     type: String,
     required: true,
   },
-  guardian: GuardianSchema,
+  guardian: {
+    type: GuardianSchema,
+    required: true,
+  },
   isActive: {
+    type: Boolean,
+    default: false,
+  },
+  isDeleted: {
     type: Boolean,
     default: false,
   },
 });
 
-const Student = model('Student', StudentSchema);
+// hooks
+studentSchema.pre('save', async function (next) {
+  // eslint-disable-next-line @typescript-eslint/no-this-alias
+  // this.password = await bcrypt.hash(this.password, 10);
+  // next();
+
+  try {
+    // Check if the password is modified or this is a new user
+    if (!this.isModified('password') || this.isNew) {
+      const hashPassword = await bcrypt.hash(
+        this.password,
+        Number(config.bcrypt_salt),
+      );
+      this.password = hashPassword;
+    }
+    next();
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    next(error);
+  }
+});
+
+studentSchema.post('save', function (document, next) {
+  document.isDeleted = undefined!;
+  document.password = undefined!;
+  next();
+});
+
+studentSchema.statics.isUserExists = async function (id: string) {
+  const result = await Student.findOne({ id });
+  return result;
+};
+
+studentSchema.pre('find', function (next) {
+  this.find({ isDeleted: false });
+  next();
+});
+
+// studentSchema.methods.isUserExists = function (id: string) {
+//   const result = Student.findOne({ id });
+//   return result;
+// };
+
+const Student = model<IStudent, IStudentModel>('Student', studentSchema);
 
 export default Student;
