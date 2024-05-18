d3a4d0e4f4f697c959f6323baaf604cf0e0178e3
naimuddin94 <developernaimuddin@gmail.com>
2024-05-19 01:36:11 +0600

    update student.controller.ts

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -1,13 +1,27 @@
 import { Request, Response } from 'express';
 import { ApiError, ApiResponse, asyncHandler } from '../../../utils';
 import { studentService } from './student.service';
+import { studentValidationSchema } from './student.validator';
 
 const createStudent = asyncHandler(async (req: Request, res: Response) => {
   const { student } = req.body;
-  const result = await studentService.saveStudentToDB(student);
-  res
-    .status(200)
-    .json(new ApiResponse(200, result, 'Student created successfully'));
+
+  // const validationStudent = studentValidationSchema.parse(student);
+
+  const { data, error } = studentValidationSchema.safeParse(student);
+
+  if (error) {
+    throw new ApiError(
+      400,
+      error.issues[0].message,
+      error.issues,
+    );
+  }
+
+  const result = await studentService.saveStudentToDB(data);
+  return res
+    .status(201)
+    .json(new ApiResponse(201, result, 'Student created successfully'));
 });
 
 const fetchedStudents = asyncHandler(async (req: Request, res: Response) => {
@@ -37,8 +51,17 @@ const fetchedSingleStudent = asyncHandler(
   },
 );
 
+const deleteStudent = asyncHandler(async (req: Request, res: Response) => {
+  const result = await studentService.deleteStudentFromDB(req.params.id);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, result, 'Student deleted successfully'));
+});
+
 export const studentController = {
   createStudent,
   fetchedStudents,
   fetchedSingleStudent,
+  deleteStudent,
 };
