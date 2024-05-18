a2c6b2be1424bf53553326308e7b07701fbd1bd5
naimuddin94 <developernaimuddin@gmail.com>
2024-05-19 01:36:08 +0600

    update src/app/config/index.ts

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -1,10 +1,10 @@
-import dotenv from "dotenv";
-import path from "path";
+import dotenv from 'dotenv';
+import path from 'path';
 
-dotenv.config({ path: path.join(process.cwd(), "/.env") });
-
+dotenv.config({ path: path.join(process.cwd(), '.env') });
 
 export default {
   port: process.env.PORT,
   db_uri: process.env.DB_URI,
+  bcrypt_salt: process.env.BCRYPT_SALT,
 };
